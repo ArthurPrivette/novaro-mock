ab0e1165c92cf015a3f3f2ab75f69abf23f0b02a
Arthur Pinheiro Privette <apriv@unc.edu>
2025-10-23 10:21:14 +0100

    Add Disclaimer component

---
+++ components/Disclaimer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Disclaimer: React.FC = () => (
+  <p className="mt-6 text-xs text-gray-400">
+    This app does not constitute investment advice. Investing involves risks, including possible loss of principal.
+  </p>
+);
+
+export default Disclaimer;
